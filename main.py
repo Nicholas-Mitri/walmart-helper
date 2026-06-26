12e4ef285a6683363eb66cbeece008384167b399
Nicholas Mitri <nicholas.g.mitri@gmail.com>
2026-06-25 21:17:04 -0400

    Refactor main application structure by removing task logging functionality and integrating routers for activity log, pick list, and product catalog. Initialize database schema on startup with SQLAlchemy.

--- main.py
+++ main.py
@@ -1,35 +1,18 @@
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
-from pydantic import BaseModel
-from starlette import status
-from datetime import datetime as dt
-import fetch_item_info
+from models import Base
+from database import engine_sqlite
+from routers import activity_log, pick_list, product_catalog
 
 app = FastAPI()
 
-
-class TaskRequest(BaseModel):
-    item_url: str
-    assigned_to: str
-    quantity: int
-    action: str
+Base.metadata.create_all(bind=engine_sqlite)
 
 
-@app.post("/log_task/", status_code=status.HTTP_201_CREATED)
-async def log_task(request: TaskRequest):
-    try:
-        response_data = {
-            "status": "success",
-            "message": "Task added.",
-            "task_timestamp": dt.now().strftime("%Y-%m-%d %H:%M:%S"),
-        }
-        item_url = request.model_dump()["item_url"]
-        item_info = fetch_item_info.fetch(item_url)
-        response_data.update(**item_info)
-        return JSONResponse(status_code=200, content=response_data)
-    except Exception as e:
-        error_response = {
-            "status": "error",
-            "message": f"An error occurred while processing the task: {str(e)}",
-        }
-        return JSONResponse(status_code=500, content=error_response)
+@app.get("/healthy")
+async def root():
+    return {"status": "healthy"}
+
+
+app.include_router(product_catalog.router)
+app.include_router(pick_list.router)
+app.include_router(activity_log.router)
